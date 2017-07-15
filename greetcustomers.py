a58237eaa0c6bf172f486d0744d7f8758bdc599d
hoodielive <hoodielive@gmail.com>
2017-07-15 16:33:48 -0400

    greet the customers

---
+++ greetcustomers.py
@@ -0,0 +1,17 @@
+var customerData = {
+    'Joe': {
+        visits: 1
+    },
+
+    'Carol': {
+        visits: 2
+    },
+
+    'Howard': {
+        visits: 3
+    },
+
+    'Carrie': {
+        visits: 4
+    }
+};
